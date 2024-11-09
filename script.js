7fc16b3173037e7c8216723ec7280288ce33a60b
isaacnewtonthe2nd <ayotuns73@gmail.com>
2024-11-08 21:46:28 -0800

    Create a function getComputerChoice that returns the computer's move

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+function getComputerChoice () {
+  let computerChoice;
+
+  let randomNum = Math.floor(Math.random() * 100) + 1; //Generates random number between 0 and 100;
+
+  if (randomNum <= 33) {
+    computerChoice = "Rock";
+  } else if (randomNum >= 34 && randomNum <= 66) {
+    computerChoice = "Paper";
+  } else if (randomNum >= 67 && randomNum <= 100) {
+    computerChoice = "Scissors"
+  }; //Translates the random numbers generated into a computer move
+
+  return computerChoice;
+}
